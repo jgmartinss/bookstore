bb5453b289f4f7cc699f275754134b14639332fb
João Martins <jgmartinsss@hotmail.com>
2018-12-23 20:04:52 -0200

    corrigindo erro na qtd de itens no carrinho

--- bookstore/apps/checkout/urls.py
+++ bookstore/apps/checkout/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('detail/', views.cart_detail, name='cart_detail'),
+    path('cart/', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
     #path('remove/<product_id>/', views.cart_remove, name='cart_remove'),
 ]
