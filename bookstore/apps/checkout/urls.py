4626c353cb3b64c2fba1204d3af63a1542ff855e
João Martins <jgmartinsss@hotmail.com>
2018-12-28 01:49:41 -0200

    Feature: função para remover item do carrinho

--- bookstore/apps/checkout/urls.py
+++ bookstore/apps/checkout/urls.py
@@ -10,5 +10,6 @@ app_name = 'checkout'
 urlpatterns = [
     path('cart/', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
-    #path('remove/<product_id>/', views.cart_remove, name='cart_remove'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    # path('onepage/', views.cart_onepage, name='cart_onepage'),
 ]
